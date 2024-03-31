4af519b08b5edc9cbc994738f63c58d00006ee5b
Mrutunjay Panda <bapunpanda63@gmail.com>
2024-03-31 20:02:49 +0530

    ../

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,30 @@
+const path = require("path");
+const glob = require("glob");
 
+exports.initializeRoutes = (app) => {
+  const directory = path.join(__dirname);
+
+  console.log(directory);
+
+  const routeFiles = glob.sync(`${directory}/*.route.js`);
+  console.log("routeFiles", routeFiles);
+
+  if (routeFiles.length === 0) {
+    console.log("no routes found or no route files are found");
+  }
+
+  routeFiles.forEach((files) => {
+    console.log("ROUTE FILE : ", files);
+    try {
+      const absoluteRouteFilePath = path.resolve(files);
+      console.log("REQUIRED FILE : ", absoluteRouteFilePath);
+
+      const routes = require(absoluteRouteFilePath);
+      routes(app);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  console.log(`${routeFiles.length} routes initialized`);
+};
