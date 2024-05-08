bac0d00985fdfb704882b6a57c83bc1807a4a429
Mrutunjay Panda <bapunpanda63@gmail.com>
2024-05-09 03:02:36 +0530

    ..

--- controllers/shortlistController.js
+++ controllers/shortlistController.js
@@ -1,5 +1,7 @@
-const { InterestRequests, ProfileRequests } = require("../models/interests");
+const { Country, State, City, Diet, Proffesion, Community } = require("../models/masterSchemas");
 const ShortList = require("../models/shortlistUsers");
+const { ProfileRequests, InterestRequests } = require("../models/interests");
+const { getSignedUrlFromS3 } = require("../utils/s3Utils");
 const { ListData } = require("../helper/cardListedData");
 
 exports.addToShortlist = async (req, res) => {
@@ -24,11 +26,11 @@ exports.addToShortlist = async (req, res) => {
         await Promise.all([
           ProfileRequests.updateMany(
             { profileRequestBy: user, profileRequestTo: shortlistedUserId },
-            { isShortListedBy: 'no' }
+            { isShortListedBy: false }
           ),
           InterestRequests.updateMany(
             { interestRequestBy: user, interestRequestTo: shortlistedUserId },
-            { isShortListedBy: 'no' }
+            { isShortListedBy: false }
           )
         ]);
       } else {
@@ -46,11 +48,11 @@ exports.addToShortlist = async (req, res) => {
         await Promise.all([
           ProfileRequests.updateMany(
             { profileRequestBy: user, profileRequestTo: shortlistedUserId },
-            { isShortListedBy: 'yes' }
+            { isShortListedBy: true }
           ),
           InterestRequests.updateMany(
             { interestRequestBy: user, interestRequestTo: shortlistedUserId },
-            { isShortListedBy: 'yes' }
+            { isShortListedBy: true }
           ),
         ]);
       }
@@ -65,10 +67,83 @@ exports.addToShortlist = async (req, res) => {
 exports.getShortlistedUser = async (req, res) => {
     try {
       const { UserId } = req.params;
-      const user = await ShortList.find({ user: UserId }).populate({
+      const users = await ShortList.find({ user: UserId }).populate({
         path: "shortlistedUser",
         select: ListData,
       });
+      console.log(users);
+      const user = JSON.parse(JSON.stringify(users));
+  
+      // Fetch additional data for users
+      const communityIds = user.map(user => user.shortlistedUser.familyDetails[0]?.community);
+      const professionIds = user.map(user => user.shortlistedUser.careerDetails[0]?.profession);
+      const dietIds = user.map(user => user.shortlistedUser.additionalDetails[0]?.diet);
+      const countryIds = user.map(user => user.shortlistedUser.additionalDetails[0]?.currentlyLivingInCountry);
+      const stateIds = user.map(user => user.shortlistedUser.additionalDetails[0]?.currentlyLivingInState);
+      const borncountryIds = user.map(user => user.shortlistedUser.basicDetails[0]?.placeOfBirthCountry);
+      const bornstateIds = user.map(user => user.shortlistedUser.basicDetails[0]?.placeOfBirthState);
+      const cityIds = user.map(user => user.shortlistedUser.additionalDetails[0]?.currentlyLivingInCity);
+  
+      const [communities, professions, diets, countries, bornCoutnry, bornState, states, cities, shortlistedUsers, profileRequests, interestRequests] = await Promise.all([
+        Community.find({ community_id: { $in: communityIds } }),
+        Proffesion.find({ proffesion_id: { $in: professionIds } }),
+        Diet.find({ diet_id: { $in: dietIds } }),
+        Country.find({ country_id: { $in: countryIds } }),
+        Country.find({ country_id: { $in: borncountryIds } }),
+        State.find({ state_id: { $in: bornstateIds } }),
+        State.find({ state_id: { $in: stateIds } }),
+        City.find({ city_id: { $in: cityIds } }),
+        ShortList.find({ user: UserId }),
+        ProfileRequests.find({ profileRequestBy: UserId }),
+        InterestRequests.find({ interestRequestBy: UserId })
+      ]);
+  
+      const promises = user.map(async (user) => {
+        const userIdString = String(user.shortlistedUser._id);
+        const profileUrl = await getSignedUrlFromS3(user.shortlistedUser.selfDetails[0]?.profilePicture || "");
+        user.shortlistedUser.selfDetails[0].profilePictureUrl = profileUrl || "";
+      
+        if (user.shortlistedUser.familyDetails && user.shortlistedUser.familyDetails[0]?.community) {
+          const communityData = communities.find(community => community.community_id === user.shortlistedUser.familyDetails[0]?.community);
+          user.shortlistedUser.familyDetails[0].communityName = communityData?.community_name || "";
+        }
+        if (user.shortlistedUser.careerDetails && user.shortlistedUser.careerDetails[0]?.profession) {
+          const professionData = professions.find(profession => profession.proffesion_id === user.shortlistedUser.careerDetails[0]?.profession);
+          user.shortlistedUser.careerDetails[0].professionName = professionData?.proffesion_name || "";
+        }
+        if (user.shortlistedUser.additionalDetails && user.shortlistedUser.additionalDetails[0]?.diet) {
+          const dietData = diets.find(diet => diet.diet_id === user.shortlistedUser.additionalDetails[0]?.diet);
+          user.shortlistedUser.additionalDetails[0].dietName = dietData?.diet_name || "";
+        }
+        if (user.shortlistedUser.additionalDetails && user.shortlistedUser.additionalDetails[0]?.currentlyLivingInCountry) {
+          const countryData = countries.find(country => country.country_id === user.shortlistedUser.additionalDetails[0]?.currentlyLivingInCountry);
+          user.shortlistedUser.additionalDetails[0].currentCountryName = countryData?.country_name || "";
+          if (user.shortlistedUser.additionalDetails[0]?.currentlyLivingInState) {
+            const stateData = states.find(state => state.state_id === user.shortlistedUser.additionalDetails[0]?.currentlyLivingInState);
+            user.shortlistedUser.additionalDetails[0].currentStateName = stateData?.state_name || "";
+            if (user.shortlistedUser.additionalDetails[0]?.currentlyLivingInCity) {
+              const cityData = cities.find(city => city.city_id === user.shortlistedUser.additionalDetails[0]?.currentlyLivingInCity);
+              user.shortlistedUser.additionalDetails[0].currentCityName = cityData?.city_name || "";
+            }
+          }
+        }
+        if (user.shortlistedUser.basicDetails && user.shortlistedUser.basicDetails[0]?.placeOfBirthCountry) {
+          const countryData = bornCoutnry.find(country => country.country_id === user.shortlistedUser.basicDetails[0]?.placeOfBirthCountry);
+          user.shortlistedUser.basicDetails[0].currentCountryName = countryData?.country_name || "";
+          if (user.shortlistedUser.basicDetails[0]?.placeOfBirthState) {
+            const stateData = bornState.find(state => state.state_id === user.shortlistedUser.basicDetails[0]?.placeOfBirthState);
+            user.shortlistedUser.basicDetails[0].currentStateName = stateData?.state_name || "";
+          }
+        }
+  
+        // Check if there is a profile request to this user
+        user.isProfileRequest = profileRequests.some(data => String(data.profileRequestTo) === userIdString);
+  
+        // Check if there is an interest request to this user
+        user.isInterestRequest = interestRequests.some(data => String(data.interestRequestTo) === userIdString);
+      });
+      
+      await Promise.all(promises);
       if (!user) {
         return res.status(404).json({ message: "User not found" });
       }
--- models/interests.js
+++ models/interests.js
@@ -4,8 +4,8 @@ const profileRequestSchema = mongoose.Schema({
   profileRequestBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   profileRequestTo: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   action: { type: String, enum:["pending","accepted","declined", "cancelled", "blocked"] },
-  isShortListedTo: { type: String, enum:["no", "yes"], default : "no" },
-  isShortListedBy: { type: String, enum:["no", "yes"], default : "no" },
+  isShortListedTo: { type: String, enum:["no", "yes", true, false], default : false },
+  isShortListedBy: { type: String, enum:["no", "yes", true, false], default : false },
   isBlocked : {
     type: String,
     enum: [true, false],
@@ -23,8 +23,8 @@ const interestRequestSchema = mongoose.Schema({
   interestRequestBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   interestRequestTo: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   action: { type: String, enum:["pending","accepted","declined", "cancelled", "blocked"] },
-  isShortListedTo: { type: String, enum:["no", "yes"], default : "no" },
-  isShortListedBy: { type: String, enum:["no", "yes"], default : "no" },
+  isShortListedTo: { type: String, enum:["no", "yes", true, false], default : false },
+  isShortListedBy: { type: String, enum:["no", "yes", true, false], default : false },
   isBlocked : {
     type: String,
     enum: [true, false],
