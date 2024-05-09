349d7eca5f942d3a3894daf2bd7b9b83299d0657
Mrutunjay Panda <bapunpanda63@gmail.com>
2024-05-10 00:14:26 +0530

    ..return interest image

--- controllers/requestController.js
+++ controllers/requestController.js
@@ -165,7 +165,12 @@ exports.blockProfileRequest = async (req, res) => {
 exports.getProfileRequestsAccepted = async (req, res) => {
   try {
     const { userId } = req.params;
-    await getRequests(ProfileRequests, userId, "Profile", "accepted", res);
+    const requests = await getRequests(ProfileRequests, userId, "Profile", "accepted", res);
+    await Promise.all(requests.map(async (item) => {
+      item.publicRequestTo.selfDetails[0].profilePictureUrl = await getSignedUrlFromS3(item?.publicRequestTo?.selfDetails[0]?.profilePicture);
+      item.publicRequestBy.selfDetails[0].profilePictureUrl = await getSignedUrlFromS3(item?.publicRequestBy?.selfDetails[0]?.profilePicture);
+    }));
+    return res.status(200).json({ requests });
   } catch (error) {
     console.error("Error getting accepted profile requests:", error);
     res.status(500).json({ error: "Internal Server Error" });
@@ -175,7 +180,13 @@ exports.getProfileRequestsAccepted = async (req, res) => {
 exports.getProfileRequestsDeclined = async (req, res) => {
   try {
     const { userId } = req.params;
-    await getRequests(ProfileRequests, userId, "Profile", "declined", res);
+    const requests = await getRequests(ProfileRequests, userId, "Profile", "declined", res);
+    // Fetch profile picture URLs for each request
+    await Promise.all(requests.map(async (item) => {
+      item.publicRequestTo.selfDetails[0].profilePictureUrl = await getSignedUrlFromS3(item?.publicRequestTo?.selfDetails[0]?.profilePicture);
+      item.publicRequestBy.selfDetails[0].profilePictureUrl = await getSignedUrlFromS3(item?.publicRequestBy?.selfDetails[0]?.profilePicture);
+    }));
+    return res.status(200).json({ requests });
   } catch (error) {
     console.error("Error getting declined profile requests:", error);
     res.status(500).json({ error: "Internal Server Error" });
@@ -185,7 +196,12 @@ exports.getProfileRequestsDeclined = async (req, res) => {
 exports.getProfileRequestsSent = async (req, res) => {
   try {
     const { userId } = req.params;
-    await getPendingRequests(ProfileRequests, userId, "Profile", res);
+    const requests = await getPendingRequests(ProfileRequests, userId, "Profile", res);
+    // Fetch profile picture URLs for each request
+    await Promise.all(requests.map(async (item) => {
+      item.profileRequestTo.selfDetails[0].profilePictureUrl = await getSignedUrlFromS3(item?.profileRequestTo?.selfDetails[0]?.profilePicture);
+    }));
+    return res.status(200).json({ requests });
   } catch (error) {
     console.error("Error getting pending profile requests:", error);
     res.status(500).json({ error: "Internal Server Error" });
@@ -195,7 +211,12 @@ exports.getProfileRequestsSent = async (req, res) => {
 exports.getProfileRequestsReceived = async (req, res) => {
   try {
     const { userId } = req.params;
-    await getPendingRequests(ProfileRequests, userId, "Profile", res, true);
+    const requests = await getPendingRequests(ProfileRequests, userId, "Profile", res, true);
+    // Fetch profile picture URLs for each request
+    await Promise.all(requests.map(async (item) => {
+      item.profileRequestBy.selfDetails[0].profilePictureUrl = await getSignedUrlFromS3(item?.profileRequestBy?.selfDetails[0]?.profilePicture);
+    }));
+    return res.status(200).json({ requests });
   } catch (error) {
     console.error("Error getting pending profile requests:", error);
     res.status(500).json({ error: "Internal Server Error" });
@@ -364,7 +385,13 @@ exports.blockedInterestRequest = async (req, res) => {
 exports.getInterestRequestsAccepted = async (req, res) => {
   try {
     const { userId } = req.params;
-    await getRequests(InterestRequests, userId, "Interest", "accepted", res);
+    const requests = await getRequests(InterestRequests, userId, "Interest", "accepted", res);
+    // Fetch profile picture URLs for each request
+    await Promise.all(requests.map(async (item) => {
+      item.interestRequestTo.selfDetails[0].profilePictureUrl = await getSignedUrlFromS3(item?.interestRequestTo?.selfDetails[0]?.profilePicture);
+      item.interestRequestBy.selfDetails[0].profilePictureUrl = await getSignedUrlFromS3(item?.interestRequestBy?.selfDetails[0]?.profilePicture);
+    }));
+    return res.status(200).json({ requests });
   } catch (error) {
     console.error("Error getting accepted interest requests:", error);
     res.status(500).json({ error: "Internal Server Error" });
@@ -374,7 +401,13 @@ exports.getInterestRequestsAccepted = async (req, res) => {
 exports.getInterestRequestsDeclined = async (req, res) => {
   try {
     const { userId } = req.params;
-    await getRequests(InterestRequests, userId, "Interest", "declined", res);
+    const requests = await getRequests(InterestRequests, userId, "Interest", "declined", res);
+    // Fetch profile picture URLs for each request
+    await Promise.all(requests.map(async (item) => {
+      item.interestRequestTo.selfDetails[0].profilePictureUrl = await getSignedUrlFromS3(item?.interestRequestTo?.selfDetails[0]?.profilePicture);
+      item.interestRequestBy.selfDetails[0].profilePictureUrl = await getSignedUrlFromS3(item?.interestRequestBy?.selfDetails[0]?.profilePicture);
+    }));
+    return res.status(200).json({ requests });
   } catch (error) {
     console.error("Error getting declined interest requests:", error);
     res.status(500).json({ error: "Internal Server Error" });
@@ -384,7 +417,12 @@ exports.getInterestRequestsDeclined = async (req, res) => {
 exports.getInterestRequestsSent = async (req, res) => {
   try {
     const { userId } = req.params;
-    await getPendingRequests(InterestRequests, userId, "Interest", res);
+    const requests = await getPendingRequests(InterestRequests, userId, "Interest", res);
+    // Fetch profile picture URLs for each request
+    await Promise.all(requests.map(async (item) => {
+      item.interestRequestTo.selfDetails[0].profilePictureUrl = await getSignedUrlFromS3(item?.interestRequestTo?.selfDetails[0]?.profilePicture);
+    }));
+    return res.status(200).json({ requests });
   } catch (error) {
     console.error("Error getting pending interest requests:", error);
     res.status(500).json({ error: "Internal Server Error" });
@@ -394,7 +432,12 @@ exports.getInterestRequestsSent = async (req, res) => {
 exports.getInterestRequestsReceived = async (req, res) => {
   try {
     const { userId } = req.params;
-    await getPendingRequests(InterestRequests, userId, "Interest", res, true);
+    const requests = await getPendingRequests(InterestRequests, userId, "Interest", res, true);
+    // Fetch profile picture URLs for each request
+    await Promise.all(requests.map(async (item) => {
+      item.interestRequestBy.selfDetails[0].profilePictureUrl = await getSignedUrlFromS3(item?.interestRequestBy?.selfDetails[0]?.profilePicture);
+    }));
+    return res.status(200).json({ requests });
   } catch (error) {
     console.error("Error getting pending interest requests:", error);
     res.status(500).json({ error: "Internal Server Error" });
@@ -472,7 +515,7 @@ async function getPendingRequests(Model, userId, type, res, received = false) {
     path: `${type.toLowerCase()}Request${received ? "By" : "To"}`,
     select: ListData,
   });
-  res.status(200).json({ requests });
+  return requests;
 }
 
 async function getRequests(Model, userId, type, status, res) {
@@ -498,7 +541,7 @@ try {
       ]);
     }
 
-    return res.status(200).json({ requests });
+    return requests;
   } catch (error) {
     console.error(`Error getting ${status} ${type} requests:`, error);
     res.status(500).json({ error: "Internal Server Error" });
