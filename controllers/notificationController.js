05acf07fa0b31e27e20c7c1154dba462dd3701ed
Mrutunjay Panda <bapunpanda63@gmail.com>
2024-05-11 18:07:16 +0530

    a little left out logic to visit later

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -15,3 +15,21 @@ exports.getNotificationsForUser = async (req, res) => {
       res.status(500).json({ error: "Internal Server Error" });
     }
   };
+
+
+
+exports.notificationsSeen = async (req, res) => {
+    try {
+      const userId = req.params.userId;
+  
+      // Find notifications where the userId is either in notificationTo or notificationBy
+      const notifications = await Notifications.find({
+        $or: [{ notificationTo: userId }, { notificationBy: userId }],
+      });
+  
+      res.status(200).json(notifications);
+    } catch (error) {
+      console.error("Error fetching notifications:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  };
--- models/notifications.js
+++ models/notifications.js
@@ -4,7 +4,8 @@ const notificationSchema = mongoose.Schema({
     notificationBy : { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     notificationTo: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     notificationText: { type: String , default : ""},
-    notificationType : {type: String , default : "sent", enum :["", "profilesent", "profileaccepted", "interestsent", "interestaccepted"]}
+    notificationType : {type: String , default : "sent", enum :["", "profilesent", "profileaccepted", "interestsent", "interestaccepted"]},
+    notificationView : {type: String , default : "unseen", enum :["seen", "unseen"]}
 });
 
 const Notifications = mongoose.model("notifications", notificationSchema);
