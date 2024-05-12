7fc664dc644cb3bc231499e4eeae066344db17a5
Mrutunjay Panda <bapunpanda63@gmail.com>
2024-05-12 17:45:24 +0530

    ..admin related change

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,7 +1,8 @@
 const { updateRegistrationPhase } = require('../controllers/admin');
+const { getAllPendingUsers } = require('../controllers/auth');
 const { isAdmin } = require('../middleware/is_auth');
 
 module.exports = (app) => {
   app.put("/approve-or-decline/:userId", isAdmin, updateRegistrationPhase);
-  app.get("/get-user-data-admin/:userId", isAdmin, updateRegistrationPhase);
+  app.get("/get-user-data-admin/:userId", isAdmin, getAllPendingUsers);
 };
