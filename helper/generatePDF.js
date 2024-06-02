3aa70a84463ea7cdd8a001983edc600ae2de21b9
Mrutunjay Panda <bapunpanda63@gmail.com>
2024-06-03 02:46:02 +0530

    have done changes in pdf

--- helper/generatePDF.js
+++ helper/generatePDF.js
@@ -89,3 +89,194 @@ exports.generateUserPDFForAdmin = async (user) => {
     });
   });
 };
+
+
+
+
+// const puppeteer = require('puppeteer');
+// const ejs = require('ejs');
+// const path = require('path');
+// const { promisify } = require('util');
+
+// const fs = require('fs');
+// // const pdfMake = require('pdfmake');
+// const PDFDocument = require('pdfkit');
+
+// async function generatePdfFromHtml(htmlContent) {
+//   try {
+//     const fonts = {
+//       Roboto: {
+//         normal: path.join(__dirname, '..', 'templates', 'fonts', 'Roboto-Regular.ttf'),
+//         bold: path.join(__dirname, '..', 'templates', 'fonts', 'Roboto-Bold.ttf'),
+//         italics: path.join(__dirname, '..', 'templates', 'fonts', 'Roboto-Italic.ttf'),
+//         bolditalics: path.join(__dirname, '..', 'templates', 'fonts', 'Roboto-BoldItalic.ttf')
+//       }
+//     };
+
+//     const printer = new pdfMake(fonts);
+
+//     const docDefinition = {
+//       content: [
+//         { text: 'Dynamic HTML to PDF', style: 'header' },
+//         { text: htmlContent, style: 'body' }
+//       ],
+//       styles: {
+//         header: { fontSize: 18, bold: true },
+//         body: { fontSize: 12 }
+//       }
+//     };
+
+//     const pdfDoc = printer.createPdfKitDocument(docDefinition);
+
+//     const buffers = [];
+//     pdfDoc.on('data', buffer => buffers.push(buffer));
+//     pdfDoc.on('end', () => {
+//       const pdfBuffer = Buffer.concat(buffers);
+//       fs.writeFileSync('output.pdf', pdfBuffer);
+//     });
+
+//     pdfDoc.end();
+//   } catch (error) {
+//     console.error('Error generating PDF:', error);
+//     throw new Error('Error generating PDF');
+//   }
+// }
+// async function generatePdfFromHtml(htmlContent) {
+//   try {
+//     const pdfDoc = new PDFDocument();
+//     const buffers = [];
+
+//     pdfDoc.on('data', buffer => buffers.push(buffer));
+//     pdfDoc.on('end', () => {
+//       const pdfBuffer = Buffer.concat(buffers);
+//       // Optionally, you can write the PDF to a file
+//       fs.writeFileSync('output.pdf', pdfBuffer);
+//     });
+
+//     // Pipe the HTML content to the PDF document
+//     pdfDoc.text(htmlContent);
+//     pdfDoc.end();
+
+//     return new Promise((resolve, reject) => {
+//       pdfDoc.on('end', () => {
+//         const pdfBuffer = Buffer.concat(buffers);
+//         resolve(pdfBuffer);
+//       });
+//       pdfDoc.on('error', (err) => {
+//         reject(err);
+//       });
+//     });
+//   } catch (error) {
+//     console.error('Error generating PDF:', error);
+//     throw new Error('Error generating PDF');
+//   }
+// }
+// exports.generateUserPDFForAdmin = async (user) => {
+//   try {
+
+//     let userData = mapSchemaToUserData(user);
+//     console.log(userData);
+//     const htmlContent = await promisify(ejs.renderFile)(
+//       path.join(__dirname, '..','templates', 'userTemplate.ejs'),
+//       userData
+//     );
+//     const pdfBuffer = await generatePdfFromHtml(htmlContent);
+
+//     // console.log("gotOut1", htmlContent);
+
+//     // const browser = await puppeteer.launch({
+//     //   timeout: 0 // Setting timeout to 0 means no timeout
+//     // });
+//     // console.log("gotOut2", browser);
+//     // const page = await browser.newPage();
+//     // console.log("gotOut3");
+//     // await page.setContent(htmlContent, { waitUntil: 'domcontentloaded' });
+//     // console.log("gotOut4", page);
+//     // const pdfBuffer = await page.pdf({path : 'output.pdf', format : 'A4'});
+//     // console.log("gotOut5");
+    
+//     // console.log(pdfBuffer);
+//     // console.log("gotOut6");
+//     // await browser.close();
+//     // console.log("gotOut7");
+//     return pdfBuffer;
+
+//   } catch (error) {
+//     console.error('Error generating PDF:', error);
+//     throw new Error('Error generating PDF');
+//   }
+// };
+
+
+const mapSchemaToUserData = (schemaData) => {
+  const basicDetails = schemaData.basicDetails || {};
+  const additionalDetails = schemaData.additionalDetails || {};
+  const careerDetails = schemaData.careerDetails || {};
+  const familyDetails = schemaData.familyDetails || {};
+  const selfDetails = schemaData.selfDetails || {};
+
+  const userData = {
+    name: basicDetails.name || "",
+    userId: schemaData.userId || "",
+    profilePictureUrl: selfDetails.profilePictureUrl || "",
+    age0: basicDetails.age || "",
+    height0: additionalDetails.height ? additionalDetails.height.toString() : "",
+    dateofBirth0: basicDetails.dateOfBirth || "",
+    maritalStatus0: additionalDetails.maritalStatus || "",
+    profession0: careerDetails.profession || "",
+    currentlyLivingInCity: additionalDetails.currentlyLivingInCity || "",
+    currentlyLivingInState: additionalDetails.currentlyLivingInState || "",
+    religion: familyDetails.religion || "",
+    timeofBirth0: basicDetails.timeOfBirth || "",
+    diet0: additionalDetails.diet || "",
+    community0: familyDetails.community || "",
+    photoGrid: "", // Assuming this data is stored elsewhere or constructed dynamically
+    aboutYourself: selfDetails.aboutYourself || "",
+    personalAppearance: additionalDetails.personalAppearance || "",
+    name1: basicDetails.name || "",
+    gender: basicDetails.gender || "",
+    dateofBirth: basicDetails.dateOfBirth || "",
+    timeofBirth: basicDetails.timeOfBirth || "",
+    age: basicDetails.age || "",
+    palceofBirthCity: basicDetails.placeOfBirthCity || "",
+    palceofBirthState: basicDetails.placeOfBirthState || "",
+    palceofBirthCountry: basicDetails.placeOfBirthCountry || "",
+    height: additionalDetails.height ? additionalDetails.height.toString() : "",
+    weight: additionalDetails.weight || "",
+    currentlyLivingInCountry: additionalDetails.currentlyLivingInCountry || "",
+    currentlyLivingInState1: additionalDetails.currentlyLivingInState || "",
+    currentlyLivingInCity1: additionalDetails.currentlyLivingInCity || "",
+    relocationInFuture: additionalDetails.relocationInFuture || "",
+    diet: additionalDetails.diet || "",
+    alcohol: additionalDetails.alcohol || "",
+    smoking: additionalDetails.smoking || "",
+    maritalStatus: additionalDetails.maritalStatus || "",
+    contact: additionalDetails.contact || "",
+    email: additionalDetails.email || "",
+    educationName: careerDetails.highestQualification || "",
+    university: careerDetails["school/university"] || "",
+    highestQualification: careerDetails.highestQualification || "",
+    profession: careerDetails.profession || "",
+    currentDesignation: careerDetails.currentDesignation || "",
+    previousOccupation: careerDetails.previousOccupation || "",
+    annualIncomeValue: careerDetails.annualIncomeValue || "",
+    userPhotos : selfDetails.userPhotosUrl.filter(photoUrl => photoUrl !== null) || [],
+    interestsTypes: selfDetails.interests || "",
+    funActivitiesTypes: selfDetails.fun || "",
+    fitnessTypes: selfDetails.fitness || "",
+    otherTypes: selfDetails.other || "",
+    fatherName: familyDetails.fatherName || "",
+    fatherOccupation: familyDetails.fatherOccupation || "",
+    motherName: familyDetails.motherName || "",
+    motherOccupation: familyDetails.motherOccupation || "",
+    siblingGrid: "", // Assuming this data is stored elsewhere or constructed dynamically
+    withFamilyStatus: familyDetails.withFamilyStatus || "",
+    familyLocationCountry: familyDetails.familyLocationCountry || "",
+    familyLocationState: familyDetails.familyLocationState || "",
+    familyLocationCity: familyDetails.familyLocationCity || "",
+    community: familyDetails.community || "",
+    familyAnnualIncomeStart: familyDetails.familyAnnualIncomeStart || ""
+  };
+
+  return userData;
+};
