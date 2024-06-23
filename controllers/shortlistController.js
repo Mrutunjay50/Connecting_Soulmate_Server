8a3d5ee74e33c8f7fc5d975bd4216e74de399a8c
Mrutunjay Panda <bapunpanda63@gmail.com>
2024-06-24 01:14:12 +0530

    done with updating and adding pagination to the shortlisted data

--- controllers/shortlistController.js
+++ controllers/shortlistController.js
@@ -84,11 +84,18 @@ exports.addToShortlist = async (req, res) => {
 exports.getShortlistedUser = async (req, res) => {
     try {
       const { userId } = req.params;
-      const users = await ShortList.find({ user: userId }).populate({
-        path: "shortlistedUser",
-        select: ListData,
-      });
-      // console.log(users);
+      const page = parseInt(req.query.page) || 1;
+      const PAGE_LIMIT = parseInt(req.query.limit) || 10;
+      const skip = (page - 1) * PAGE_LIMIT;
+
+      // Fetch the shortlisted users with pagination
+      const users = await ShortList.find({ user: userId })
+        .skip(skip)
+        .limit(PAGE_LIMIT)
+        .populate({
+          path: 'shortlistedUser',
+          select: ListData,
+        });
       const user = JSON.parse(JSON.stringify(users));
   
       // Fetch additional data for users
