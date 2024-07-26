1a6850e1eb43bc3fc3945e01c81adbb0a2a49f4e
Mrutunjay Panda <bapunpanda63@gmail.com>
2024-07-27 00:44:19 +0530

    resolving conflicts

--- routes/adminDashboardTab.route.js
+++ routes/adminDashboardTab.route.js
@@ -9,6 +9,8 @@ const {
   getTotalUsersUnCategorised,
   getTotalActiveUsers,
   getSuccessfulMarriages,
+  // getBannedUsers,
+  // getRejectedUsers
 } = require("../controllers/adminDashboardTabs");
 const { isAdmin } = require("../middleware/is_auth");
 
@@ -23,4 +25,6 @@ module.exports = (app) => {
   app.get("/total-users-uncategorised", getTotalUsersUnCategorised);
   app.get("/total-active-users", getTotalActiveUsers);
   app.get("/total-successful-marriages", getSuccessfulMarriages);
+  // app.get("/total-banned-users", getBannedUsers);
+  // app.get("/total-rejected-users", getRejectedUsers);
 };
