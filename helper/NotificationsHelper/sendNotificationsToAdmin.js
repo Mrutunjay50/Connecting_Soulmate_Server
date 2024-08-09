d30d1ce79b27f12eaa7fd7fb972965b750d1f41e
Mrutunjay Panda <bapunpanda63@gmail.com>
2024-08-09 20:03:43 +0530

    done with the changes for user notifications on admin side

--- helper/NotificationsHelper/sendNotificationsToAdmin.js
+++ helper/NotificationsHelper/sendNotificationsToAdmin.js
@@ -1,12 +1,13 @@
 const User = require("../../models/Users");
 const io = require("../../socket");
+const { events } = require("../../utils/eventsConstants");
 
 exports.sendNotificationToAdmins = async (formattedNotification) => {
     try {
       const adminAndSpecialUsers = await User.find({ accessType: { $in: [0, 1] } }).select('_id');
   
       adminAndSpecialUsers.forEach(user => {
-        io.getIO().emit(`notification/${user._id}`, formattedNotification);
+        io.getIO().emit(`${events.NOTIFICATION}/${user._id}`, formattedNotification);
       });
     } catch (error) {
       console.error("Error sending notification to admins:", error);
