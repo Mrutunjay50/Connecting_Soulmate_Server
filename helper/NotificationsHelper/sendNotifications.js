220ae5a2643590c64c4319964a4c3919dd7318a6
mrutunjay50 <pmrutunjay928@gmail.com>
2024-09-29 01:35:56 +0530

    add the integration of onesignal rest api

--- helper/NotificationsHelper/sendNotifications.js
+++ helper/NotificationsHelper/sendNotifications.js
@@ -1,6 +1,25 @@
+const axios = require('axios');
+const dotenv = require("dotenv");
 const User = require("../../models/Users");
 const io = require("../../socket");
 const { events } = require("../../utils/eventsConstants");
+
+dotenv.config();
+
+// Function to send notifications via OneSignal API
+const sendPushNotification = async (data) => {
+    try {
+      const response = await axios.post('https://api.onesignal.com/notifications', data, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Basic ${process.env.ONESIGNAL_API_KEY}`, // REST API Key
+        },
+      });
+      console.log('Push notification sent successfully:', response.data);
+    } catch (error) {
+      console.error('Error sending push notification:', error.response.data);
+    }
+};
 
 exports.sendNotificationToAdmins = async (formattedNotification) => {
     try {
@@ -9,6 +28,17 @@ exports.sendNotificationToAdmins = async (formattedNotification) => {
       adminAndSpecialUsers.forEach(user => {
         io.getIO().emit(`${events.NOTIFICATION}/${user._id}`, formattedNotification);
       });
+
+      // OneSignal notification payload
+      const notificationData = {
+        app_id: process.env.ONESIGNAL_APP_ID,
+        // contents: { en: formattedNotification },
+        contents: { en: 'You have a new notification' },
+        include_aliases : {"external_id": externalUserIds},
+      };
+  
+      // Send notification
+      await sendPushNotification(notificationData);
     } catch (error) {
       console.error("Error sending notification to admins:", error);
     }
@@ -18,10 +48,17 @@ exports.sendNotificationForChatInitiation = async (formattedNotification, reques
     try {
       // Set up a 2-second delay to trigger the INITIATE_CHAT_WITH_USER event
       setTimeout(() => {
-        console.log(events.INITIATECHATWITHUSER);
         io.getIO().emit(`${events.INITIATECHATWITHUSER}/${requestBy}`, formattedNotification);
         io.getIO().emit(`${events.INITIATECHATWITHUSER}/${requestTo}`, formattedNotification);
       }, 2000); // 2000 ms = 2 seconds
+
+      const notificationData = {
+        app_id: process.env.ONESIGNAL_APP_ID,
+        // contents: { en: formattedNotification },
+        contents: { en: 'You can now initiate a chat with a user' },
+        include_aliases : {"external_id": [requestBy]},
+      };
+      await sendPushNotification(notificationData);
     } catch (error) {
       console.error("Error sending notification to users for chatInitiation:", error);
     }
@@ -49,6 +86,19 @@ exports.sendNotificationOnNewMessage = async (data) => {
         };
         console.log(events.ONMESSAGENOTIFICATION);
         io.getIO().emit(`${events.ONMESSAGENOTIFICATION}/${data.reciever}`, formattedNotificationData);
+        // Format the notification message
+        const messageContent = `${sender.basicDetails.firstName} sent you a message: ${data.message}`;
+
+        // OneSignal notification payload
+        const notificationData = {
+          app_id: process.env.ONESIGNAL_APP_ID,
+        //   contents: { en: messageContent },
+          contents: { en: `You have a new message: ${data.message}` },
+          include_aliases : {"external_id": [data.reciever]},
+        };
+
+        // Send notification
+        await sendPushNotification(notificationData);
     } catch (error) {
         console.error("Error sending notification to the reciever:", error);
     }
